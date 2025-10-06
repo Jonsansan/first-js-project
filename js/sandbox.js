f3cc8e39f44f8dbddfba9226cacc94031fb325f5
Jon Sanchez Santiago <127Jsanchezsantiago@frhsd.com>
2025-10-06 12:50:28 -0400

    initialize project files with HTML structure, CSS, and JavaScript alert

---
+++ js/sandbox.js
@@ -0,0 +1,19 @@
+/** @format */
+/*
+// log things to the console
+console.log(1);
+console.log(2);
+
+let age = 25;
+let year = 2019;
+console.log(age, year);
+
+age = 30;
+console.log(age);
+const point = 100;
+console.log(point);
+
+var score = 75;
+console.log(score);
+*/
+alert("hello from the sandbox");
