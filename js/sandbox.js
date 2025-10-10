66a5c563de4c10833ea1b15544f7e68527168c0e
Jon Sanchez Santiago <127Jsanchezsantiago@frhsd.com>
2025-10-10 12:47:25 -0400

    refactor: enhance code examples with template strings and array methods

--- js/sandbox.js
+++ js/sandbox.js
@@ -53,33 +53,22 @@ let result = userEmail.replaceAll("n", "w");
 
 console.log(result);
 
-*/
+
 /*
 // math below
-
-let radius = 10;
-const pi = 3.14;
+*/
 //console.log(radius, pi);
 // console.log(radius, pi);
 // math operators +, -, *, /, **, %
 //console.log(10 / 2);
 //let result = radius % 3;
 
-let result = 5 * (10 - 3) ** 2;
+//let result = 5 * (10 - 3) ** 2;
 //let result = radius * 2 + pi;
 //let result = radius % 3;
 //console.log(result);
 
-
-
-
-
-
-
-
-*/
-
-let likes = 10;
+//let likes = 10;
 //likes = likes + 1;
 //likes++;
 
@@ -87,5 +76,144 @@ let likes = 10;
 
 //console.log(result);
 
-likes += 10;
-console.log(likes);
+//likes += 10;
+//console.log(likes);
+
+//let likes = 10;
+
+//let result = "The blog has " + likes + " likes";
+//console.log(result);
+
+//let result = "the blog called" + title + "by" + author + "has" + likes + "likes";
+/*
+ */
+
+/*
+// template strings
+const title = "Best reads of 2019";
+const author = "Mario";
+const likes = 30;
+
+let result = `The blog called ${title} by ${author} has ${likes} likes`;
+
+console.log(result);
+
+let html = `
+    <h2>${title}</h2>
+    <p>By ${author}</p>
+    <span>This blog has ${likes} likes</span>
+`;
+console.log(html);
+
+
+
+//ninjas[1] = "ken";
+
+//console.log(ninjas[1]);
+
+//let ages = [20, 25, 30, 35];
+//console.log(ages[2]);
+
+
+
+
+
+let ramdom = ["shaun", "crystal", 30, 20];
+console.log(random);]
+
+
+
+let ninjas = ["shaun", "ryu", "chun-li"];
+//console.log(ninjas.length);
+
+//let result = ninjas.join("-");
+//let result = ninjas.indexOf("chun-li");
+
+//let result = ninjas.concat(["ken", "yoshi"]);
+
+let result = ninjas.push("ken");
+
+result = ninjas.pop();
+
+
+
+
+
+
+
+
+
+
+console.log(result);
+
+*/ //move when done
+
+// null & undefined
+/*
+let age = null;
+
+console.log(age, age + 3, `the age is ${age}`);
+
+*/
+//booleans & comparisons
+/* console.log(true, false, "true", "false");
+
+let email = "luigi@thenetninja.co.uk";
+
+let names = ["mario", "luigi", "toad"]; */
+
+//let result = names.includes("bower");
+
+//let result = email.includes("!");
+
+//console.log(result);
+
+/* let age = 25;
+
+/*
+    console.log(age == 25);
+    console.log(age == 25);
+    console.log(age == 30);
+    console.log(age != 30);
+    console.log(age != 25);
+    console.log(age > 20);
+    console.log(age < 20);
+    console.log(age <= 25);
+    console.log(age >= 25); 
+
+let name = "shawn";
+
+console.log(name == "shawn");
+console.log(name == "Shawn");
+console.log(name > "crystal");
+console.log(name > "Crystal");
+console.log(name > "Shaun");
+
+console.log(age != 25);
+console.log(age != "25");
+
+
+let age = 25;
+
+console.log(age === 25);
+console.log(age === "25");
+console.log(age !== 25);
+console.log(age !== "25");
+
+ */
+
+let scoure = "100";
+
+//scoure = Number(scoure);
+
+/* console.log(scoure + 1);
+
+console.log(typeof scoure); */
+
+//let result = Number("hello");
+
+//let result = string(50);
+//let result = Boolean(100);
+let result = Boolean("");
+
+console.log(result);
